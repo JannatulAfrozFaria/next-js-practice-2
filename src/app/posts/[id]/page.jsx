f26f11718605193421ae24f132d7b922b555b9e8
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:47:27 +0600

    post details page design updated

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const PostDetailsPage = ({ params }) => {
     console.log(params.id);
     return (
-        <div>
-            <div key={post.id} className='border-2 border-yellow-800 rounded-lg p-6'>
+        <div className='w-3/4 mx-auto my-16'>
+            <div className='border-2 border-yellow-800 rounded-lg p-6'>
                 <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
                 <p>{post.body} </p>
                 <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
