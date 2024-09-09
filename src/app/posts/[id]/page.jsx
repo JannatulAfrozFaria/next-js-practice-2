3806c84d901ce424b0d8455e5e14b92851fca0b8
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:46:20 +0600

    layout of post details page modified

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const PostDetailsPage = ({params}) => {
+const PostDetailsPage = ({ params }) => {
     console.log(params.id);
     return (
         <div>
-             <div className="grid grid-cols-4 gap-4 mb-8">
-                <div key={post.id} className='border-2 border-yellow-800 rounded-lg p-6'>
-                    <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
-                    <p>{post.body} </p>
-                    <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
-                </div>
+            <div key={post.id} className='border-2 border-yellow-800 rounded-lg p-6'>
+                <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
+                <p>{post.body} </p>
+                <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
             </div>
         </div>
     );
