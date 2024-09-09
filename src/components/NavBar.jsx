533c0d39162ea8a7fd91cff29fdd1295c56e2c49
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 03:32:44 +0600

    navbar style modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -69,9 +69,9 @@ const NavBar = () => {
         )
     return (
         <div>
-            <nav className="bg-sky-600 p-6 flex justify-between items-center">
-                <h6 className="text-3xl text-sky-200"> <span className="text-purple-200">Next</span> Practice</h6>
-                <ul className="flex gap-4 justify-between items-center text-white">
+            <nav className="bg-yellow-500 p-6 flex justify-between items-center">
+                <h6 className="text-3xl text-sky-200"> <span className="text-purple-200">Next</span> Practice <span className="text-purple-300">2</span> </h6>
+                <ul className="flex gap-4 justify-between items-center text-yellow-950">
                     {
                         links?.map((link)=> <Link className={`${pathName === link.path && "text-purple-200 font-semibold" }`} key={link.path} href={link.path} > {link.title} </Link> )
                     }
