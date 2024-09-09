afb9efd9dd47a69a2aace0c7d4e621cd9d331397
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:53:10 +0600

    button modified

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -5,7 +5,7 @@ const PostDetailsPage = ({ params }) => {
     return (
         <div className='w-3/4 mx-auto my-16'>
             <div className='border-2 border-yellow-800 rounded-lg p-8'>
-                <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
+                <h1 className='text-3xl text-yellow-600'> Details of <br /> {post.title} </h1>
                 <p>{post.body} </p>
                 <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
             </div>
