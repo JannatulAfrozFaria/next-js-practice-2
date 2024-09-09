0c79d8be63948b052280afdb2d1339ca7c14af6c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 03:20:10 +0600

    modified heading added to home

--- src/app/page.js
+++ src/app/page.js
@@ -49,6 +49,7 @@ export default function Home() {
           </a>
         </div>
       </main> */}
+       <h1>Here is the second Practice Project</h1>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
