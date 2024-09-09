de766d027bb4fb3279443ed1c15215e56ed0e3ad
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:43:46 +0600

    single post page ui created

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -4,7 +4,13 @@ const PostDetailsPage = ({params}) => {
     console.log(params.id);
     return (
         <div>
-            
+             <div className="grid grid-cols-4 gap-4 mb-8">
+                <div key={post.id} className='border-2 border-yellow-800 rounded-lg p-6'>
+                    <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
+                    <p>{post.body} </p>
+                    <button className="text-white bg-yellow-700 p-2 rounded-md">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
