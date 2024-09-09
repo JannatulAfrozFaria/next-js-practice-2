49245096147d28cf27c8969766f15bec9a6c4af0
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:56:57 +0600

    post details page updated

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const PostDetailsPage = ({ params }) => {
     console.log(params.id);
     return (
-        <div className='w-3/4 mx-auto my-16'>
+        <div className='w-3/4 mx-auto my-16 text-center'>
             <div className='border-2 border-yellow-800 rounded-lg p-8'>
-                <h1 className='text-3xl text-yellow-600'> Details of <br /> {post.title} </h1>
-                <p>{post.body} </p>
-                <button className="text-white bg-yellow-700 p-2 rounded-md">
+                <h1 className='text-3xl text-yellow-600'> Details of <br /> {params.title} </h1>
+                <p>{params.body} </p>
+                <button className=" my-6 text-white bg-yellow-700 p-2 rounded-md">
                     <Link href={'/posts'} >Go Back</Link>
                 </button>
             </div>
