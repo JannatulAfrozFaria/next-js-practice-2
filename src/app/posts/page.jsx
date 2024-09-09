99c4017f19a3e622ff1512b60e15561b8e2ead8f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:28:30 +0600

    getPosts imported from js file

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const getPosts = async() =>{
-  const res = await  fetch('https://jsonplaceholder.typicode.com/posts');
-  const data =  await res.json();
-  return data;
-//   console.log(data);
-}
+// const getPosts = async() =>{
+//   const res = await  fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data =  await res.json();
+//   return data;
+// }
 const PostPage = async() => {
     const postsData =  await getPosts();
     console.log(postsData);
