c9e3c638741f018a0e7a5cb5d697a02b0a99a76b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:27:27 +0600

    function exported

--- src/services/postApi.js
+++ src/services/postApi.js
@@ -1,6 +1,5 @@
-const getPosts = async() =>{
+export const getPosts = async() =>{
     const res = await  fetch('https://jsonplaceholder.typicode.com/posts');
     const data =  await res.json();
     return data;
-  //   console.log(data);
   }
