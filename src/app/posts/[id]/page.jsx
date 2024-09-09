fad08b8728e62df259926c1214a4b15b24112699
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:48:10 +0600

    padding modified

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -4,7 +4,7 @@ const PostDetailsPage = ({ params }) => {
     console.log(params.id);
     return (
         <div className='w-3/4 mx-auto my-16'>
-            <div className='border-2 border-yellow-800 rounded-lg p-6'>
+            <div className='border-2 border-yellow-800 rounded-lg p-8'>
                 <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
                 <p>{post.body} </p>
                 <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
