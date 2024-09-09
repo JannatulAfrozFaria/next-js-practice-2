5543c3babc78ff56a8ee9560dfbe0f748478344a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 03:22:20 +0600

    error and loading page created

---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+            <h1 className='text-red-600 text-2xl'>Common Error Display Page </h1>
+        </div>
+    );
+};
+
+export default error;
