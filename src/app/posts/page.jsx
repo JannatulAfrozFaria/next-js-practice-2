0801b30597dff3e32d51ecba1d9131cd6189cb3f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:20:08 +0600

    id inserted in key for each post

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -14,7 +14,7 @@ const PostPage = async() => {
             <h1 className='mt-8 p-6 text-4xl text-yellow-800'>Posts will be Displayed Below</h1>
             <div className="grid grid-cols-4 gap-3">
                 {postsData.map((post)=>
-                <div className='border-2 border-yellow-800 p-6'>
+                <div key={post.id} className='border-2 border-yellow-800 p-6'>
                     <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
                     <p>{post.body} </p>
                 </div>
