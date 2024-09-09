62a36545ddea919c00ae47d644f855b0b33dc7cb
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:44:50 +0600

    onCLick added to button

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -8,7 +8,7 @@ const PostDetailsPage = ({params}) => {
                 <div key={post.id} className='border-2 border-yellow-800 rounded-lg p-6'>
                     <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
                     <p>{post.body} </p>
-                    <button className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
+                    <button onClick={'/posts'} className="text-white bg-yellow-700 p-2 rounded-md">Go Back</button>
                 </div>
             </div>
         </div>
