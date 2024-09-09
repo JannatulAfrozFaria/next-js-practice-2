ad16e0172f7fc23ab87e4a5bf8785d0cab749c52
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 04:11:24 +0600

    posts page design updated

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -10,12 +10,12 @@ const PostPage = async() => {
     const postsData =  await getPosts();
     console.log(postsData);
     return (
-        <div className='h-screen'>
+        <div className='h-screen w-5/6 mx-auto text-center'>
             <h1>Posts will be Displayed Below</h1>
-            <div className="flex">
+            <div className="grid grid-cols-4 gap-3">
                 {postsData.map((post)=>
-                <div>
-                    <h1> {post.title} </h1>
+                <div className='border-2 border-yellow-800 p-6'>
+                    <h1 className='text-3xl text-yellow-600'> {post.title} </h1>
                     <p>{post.body} </p>
                 </div>
                 )}
