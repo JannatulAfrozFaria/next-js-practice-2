ed82c4762c4a7dd9659d1c21924f13a75021d46b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 03:13:18 +0600

    PostPage created

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const PostPage = () => {
     return (
         <div>
-            
+            <h1>Posts will be Displayed Below</h1>
         </div>
     );
 };
 
-export default page;
+export default PostPage;
