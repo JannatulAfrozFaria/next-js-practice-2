4dd36be7a04f524eee3947c61c8ada6a544a8cd0
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 03:59:13 +0600

    data printed in console

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const getPosts = async(req,res,next) =>{
   const res = await  fetch('https://jsonplaceholder.typicode.com/posts');
   const data =  await res.data();
-  console.log(data);
+//   console.log(data);
 }
 const PostPage = async() => {
     const postsData =  await getPosts();
+    console.log(postsData);
+}
     return (
         <div className='h-screen'>
             <h1>Posts will be Displayed Below</h1>
