e2494c82bdbccb382f27c6fe40e5d3079a84181a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 05:12:44 +0600

    async function implemented in ui

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -6,11 +6,12 @@ const getDetailsPost = async(id)=>{
     const data = await res.json();
     return data;
 }
-const PostDetailsPage = ({ params }) => {
+const PostDetailsPage = async({ params }) => {
     // console.log(params.id);
-    const postDetails = getDetailsPost(params.id);
+    const postDetails = await getDetailsPost(params.id);
+    // const {title,body} = await getDetailsPost(params.id);
     return (
-        <div className='w-3/4 mx-auto my-16 text-center'>
+        <div className='w-3/4 mx-auto my-16 text-center h-screen'>
             <div className='border-2 border-yellow-800 rounded-lg p-8'>
                 <h1 className='text-3xl text-yellow-600'> Details of <br /> {postDetails.title} </h1>
                 <p>{postDetails.body} </p>
