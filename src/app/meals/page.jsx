b02866a218ed6ec6668ca3141105c22d63b77d3b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:19:59 +0600

    text alignment set up

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MealsPage = () => {
     return (
-        <div className='my-8 w-5/6 mx-auto'>
+        <div className='my-8 w-5/6 mx-auto text-center'>
             <h1>Meals Page</h1>
         </div>
     );
