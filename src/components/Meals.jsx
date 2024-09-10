51b7e01c2a6656b7f2e2be9e9689d0744737980a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 03:32:33 +0600

    length checked before map

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -13,9 +13,9 @@ const Meals = () => {
         setSearch(e.target.value);
         console.log(e.target.value);
     }
-    if(meals?.length === 0){
-        return <h1>No Data Found.</h1>
-    }
+    // if(meals?.length === 0){
+    //     return <h1>No Data Found.</h1>
+    // }
     useEffect(()=>{
         loadData();
     },[search])
@@ -25,7 +25,7 @@ const Meals = () => {
                 <input onChange={handler} className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
                 <button onClick={() =>loadData()} className='btn-basic'>Search </button>
                 <div className='m-8 grid grid-cols-3 gap-4'>
-                    {meals?.map((meal)=>(
+                    {meals?.length> 0 && meals?.map((meal)=>(
                         <div key={meal.idMeal} className='border-2 rounded-xl p-6'>
                             <h1 className="text-2xl">{meal.strMeal} </h1>
                             <p><span className='caption-basic'>Category : </span> {meal.strCategory} </p>
