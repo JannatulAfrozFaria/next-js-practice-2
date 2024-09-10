2deb3b19c539d41375f6a7defa7438cdad6d3458
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:25:58 +0600

    input box created for meals

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -5,6 +5,9 @@ const MealsPage = () => {
         <div className='my-12 w-5/6 mx-auto text-center'>
             <h1 className='text-basic'>Meals Page</h1>
             <p>Choose meals of your choice by searching...</p>
+            <div>
+                <input type="text" placeholder='search meals here ...' />
+            </div>
         </div>
     );
 };
