fcea959385cab8ff59b7eda37df014a48f06312c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:38:43 +0600

    meals page layout created

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -7,10 +7,7 @@ const MealsPage = () => {
         <div className='my-12 w-5/6 mx-auto text-center'>
             <h1 className='text-basic'>Meals Page</h1>
             <p className='py-6'>Choose meals of your choice by searching...</p>
-            <div>
-                <input className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
-                <button className='btn-basic'>Search </button>
-            </div>
+            
         </div>
     );
 };
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Meals = () => {
     return (
         <div>
-            
+            <div>
+                <input className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
+                <button className='btn-basic'>Search </button>
+            </div>
         </div>
     );
 };
