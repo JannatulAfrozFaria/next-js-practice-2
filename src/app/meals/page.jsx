e68de7576ce69a9144ad75db8549045e80715720
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:24:45 +0600

    headline style updated

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MealsPage = () => {
     return (
         <div className='my-12 w-5/6 mx-auto text-center'>
-            <h1>Meals Page</h1>
+            <h1 className='text-basic'>Meals Page</h1>
             <p>Choose meals of your choice by searching...</p>
         </div>
     );
