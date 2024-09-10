ed9b6897f1640ed44843864d90c54501906c8c77
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 03:34:33 +0600

    try catch block added in loadData function

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,30 +2,34 @@
 import React, { useEffect, useState } from 'react';
 
 const Meals = () => {
-    const [search,setSearch] = useState();
-    const [meals,setMeals] = useState([]);
-    const loadData =  async () =>{
-        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`)
-        const data =  res.json();
-        setMeals(data.meals);
+    const [search, setSearch] = useState();
+    const [meals, setMeals] = useState([]);
+    const loadData = async () => {
+        try {
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`)
+            const data = res.json();
+            setMeals(data.meals);
+        } catch (error) {
+            console.log(error);
+        }
     }
-    const handler = (e) =>{
+    const handler = (e) => {
         setSearch(e.target.value);
         console.log(e.target.value);
     }
     // if(meals?.length === 0){
     //     return <h1>No Data Found.</h1>
     // }
-    useEffect(()=>{
+    useEffect(() => {
         loadData();
-    },[search])
+    }, [search])
     return (
         <div>
             <div>
                 <input onChange={handler} className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
-                <button onClick={() =>loadData()} className='btn-basic'>Search </button>
+                <button onClick={() => loadData()} className='btn-basic'>Search </button>
                 <div className='m-8 grid grid-cols-3 gap-4'>
-                    {meals?.length> 0 && meals?.map((meal)=>(
+                    {meals?.length > 0 && meals?.map((meal) => (
                         <div key={meal.idMeal} className='border-2 rounded-xl p-6'>
                             <h1 className="text-2xl">{meal.strMeal} </h1>
                             <p><span className='caption-basic'>Category : </span> {meal.strCategory} </p>
