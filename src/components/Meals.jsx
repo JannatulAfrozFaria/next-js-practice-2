01dc28dd5eca4c7b36ecb10202dc217d24dcb388
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:56:19 +0600

    response converted to json()

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -5,6 +5,7 @@ const Meals = () => {
     const [search,setSearch] = useState();
     const loadData =  async () =>{
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        const data =  res.json();
     }
     const handler = (e) =>{
         setSearch(e.target.value);
