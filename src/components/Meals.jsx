a25c0dc29ff63ef376b15eb4475e73f917eb6e3e
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 03:38:35 +0600

    error check added before map

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -30,12 +30,11 @@ const Meals = () => {
                 <input onChange={handler} className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
                 <button onClick={() => loadData()} className='btn-basic'>Search </button>
                 <div className='m-8 grid grid-cols-3 gap-4'>
-                    {meals?.length > 0 && meals?.map((meal) => (
+                    {meals?.length > 0 && !error && meals?.map((meal) => (
                         <div key={meal.idMeal} className='border-2 rounded-xl p-6'>
                             <h1 className="text-2xl">{meal.strMeal} </h1>
                             <p><span className='caption-basic'>Category : </span> {meal.strCategory} </p>
                             <p><span className='caption-basic'>Instructions : </span> {meal.strInstructions} </p>
-
                         </div>
                     ))}
                 </div>
