711c2b20ecf6f509db19af2f4d68adb3af8015a7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:22:45 +0600

    caption added to headline

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -4,6 +4,7 @@ const MealsPage = () => {
     return (
         <div className='my-12 w-5/6 mx-auto text-center'>
             <h1>Meals Page</h1>
+            <p>Choose meals of your choice by searching...</p>
         </div>
     );
 };
