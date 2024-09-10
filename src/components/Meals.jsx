f5460c9ed960980f4e8461ec7e5ea4be267f6358
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 03:18:09 +0600

    meal name and category added to each meal

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -24,7 +24,8 @@ const Meals = () => {
                 <div className='m-8 grid grid-cols-3 gap-4'>
                     {meals?.map((meal)=>(
                         <div key={meal.idMeal} className='border-2 rounded-xl p-6'>
-                            <h1 className="text-2xl"></h1>
+                            <h1 className="text-2xl">{meal.strMeal} </h1>
+                            <p><span>Category : </span> {meal.strCategory} </p>
                         </div>
                     ))}
                 </div>
