36636d365851e0156f16bc37ce9dd74cb6b2f5e8
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:39:27 +0600

    meals component added to meals page

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Meals from '@/components/Meals';
 import React, { useState } from 'react';
 
 const MealsPage = () => {
@@ -7,7 +8,7 @@ const MealsPage = () => {
         <div className='my-12 w-5/6 mx-auto text-center'>
             <h1 className='text-basic'>Meals Page</h1>
             <p className='py-6'>Choose meals of your choice by searching...</p>
-            
+            <Meals></Meals>
         </div>
     );
 };
