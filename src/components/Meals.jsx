dbe583bdf4e6074cb8173b22c6c469b4b5eef19f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 04:06:19 +0600

    thumbnail added to each meal

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,30 +1,31 @@
 "use client";
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const Meals = () => {
-    const [search, setSearch] = useState();
+    const [search, setSearch] = useState("a");
+    const [error, setError] = useState("");
     const [meals, setMeals] = useState([]);
-    const [error, setError] = useState('');
+
     const loadData = async () => {
         try {
-            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`)
-            const data = res.json();
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
+            const data = await res.json();
+            console.log(data.meals);
             setMeals(data.meals);
-            setError('')
+            setError("")
         } catch (error) {
-            setError('No Data Found')
+            setError("No Data Found")
         }
     }
     const handler = (e) => {
         setSearch(e.target.value);
         console.log(e.target.value);
     }
-    // if(meals?.length === 0){
-    //     return <h1>No Data Found.</h1>
-    // }
+
     useEffect(() => {
         loadData();
-    }, [search])
+    },[])
     return (
         <div>
             <div>
@@ -33,6 +34,7 @@ const Meals = () => {
                 <div className='m-8 grid grid-cols-3 gap-4'>
                     {meals?.length > 0 && !error && meals?.map((meal) => (
                         <div key={meal.idMeal} className='border-2 rounded-xl p-6'>
+                            <Image src={meal.strMealThumb}></Image>
                             <h1 className="text-2xl">{meal.strMeal} </h1>
                             <p><span className='caption-basic'>Category : </span> {meal.strCategory} </p>
                             <p><span className='caption-basic'>Instructions : </span> {meal.strInstructions} </p>
