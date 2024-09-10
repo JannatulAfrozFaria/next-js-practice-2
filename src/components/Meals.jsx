ff0d09418359454e9cb636d7cab309b65d5da069
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-11 02:45:30 +0600

    handler function initiated in input field

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 
 const Meals = () => {
     const [search,setSearch] = useState();
+    const handler = (e) =>{
+        
+    }
     return (
         <div>
             <div>
-                <input className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
+                <input onChange={handler} className='p-4 outline-none border-transparent text-slate-600' type="text" placeholder='search meals here ...' />
                 <button className='btn-basic'>Search </button>
             </div>
         </div>
