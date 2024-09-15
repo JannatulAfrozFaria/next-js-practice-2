32c8c863a246a1a3d625cf38c6f2c80fdfd14c11
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 01:36:37 +0600

    grid for image created

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,19 +5,21 @@ const page = () => {
     return (
         <div>
             <h1 className='text-3xl'>Gallery Page</h1>
-            {
-                [1, 2, 3, 4, 5]?.map((img) => (
-                    <div key={img} className="card bg-base-100 w-96 shadow-xl">
-                        <div className="card-body">
-                            <h2 className="card-title">Image Optimization</h2>
+            <div className=''>
+                {
+                    [1, 2, 3, 4, 5]?.map((img) => (
+                        <div key={img} className="card bg-base-100 w-96 shadow-xl">
+                            <div className="card-body">
+                                <h2 className="card-title">Image Optimization</h2>
+                            </div>
+                            <figure>
+                                <Image width="1920" height="1080" src={`/${img}.jpg`}
+                                    alt="Shoes"></Image>
+                            </figure>
                         </div>
-                        <figure>
-                            <Image width="1920" height="1080" src={`/${img}.jpg`}
-                                alt="Shoes"></Image>
-                        </figure>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </div>
     );
 };
