bcf8c9ae85581f7f38c2b2b205fbaaebe20ef19e
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 02:27:33 +0600

    image caption updated

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 export const metadata = {
     title: "Gallery",
-    description: "Gallery",
+    description: "Background Image Gallery",
     keywords: ["about","about page"]
   };
 const page = () => {
@@ -14,7 +14,7 @@ const page = () => {
                     [1, 2, 3, 4, 5]?.map((img,index) => (
                         <div key={img} className="card bg-yellow-100 w-96 shadow-xl">
                             <div className="card-body">
-                                <h2 className="card-title">Image Optimization ${index + 1}</h2>
+                                <h2 className="card-title">Image Optimization {index + 1}</h2>
                             </div>
                             <figure>
                                 <Image width="1920" height="1080" src={`/${img}.jpg`}
