7f61693ff4ca1ed3e22f4d7ab9f82d1746845f81
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 01:21:22 +0600

    5 images added

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,16 +5,19 @@ const page = () => {
     return (
         <div>
             <h1 className='text-3xl'>Gallery Page</h1>
-            <div className="card bg-base-100 w-96 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Image Optimization</h2>
-                    {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
-                </div>
-                <figure>
-                    <Image width="1920" height="1080" src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"></Image>
-                </figure>
-            </div>
+            {
+                [1, 2, 3, 4, 5]?.map((img) => (
+                    <div key={img} className="card bg-base-100 w-96 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">Image Optimization</h2>
+                        </div>
+                        <figure>
+                            <Image width="1920" height="1080" src={`/${img}.jpg`}
+                                alt="Shoes"></Image>
+                        </figure>
+                    </div>
+                ))
+            }
         </div>
     );
 };
