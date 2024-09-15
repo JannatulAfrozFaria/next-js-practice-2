c9e893286fde5688e6b0db3418aa402914724b58
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 01:08:33 +0600

    description removed

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
             <div className="card bg-base-100 w-96 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
                 </div>
                 <figure>
                     <Image width={600} height={500} src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
