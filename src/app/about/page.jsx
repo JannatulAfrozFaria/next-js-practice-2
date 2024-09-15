26108d2ea29cfe2bf6716b22c276acd4f99fa675
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 02:31:53 +0600

    subsets added to headland font

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Headland_One } from 'next/font/google';
-const headland = Headland_One({weight:['400'],subsets:['']});
+const headland = Headland_One({weight:['400'],subsets:['latin']});
 export const metadata = {
     title: "About",
     description: "About Page",
