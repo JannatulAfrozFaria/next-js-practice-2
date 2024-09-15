6f1818ca0b74baa0d3181aef28cd48a285e4409b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 01:09:40 +0600

    heading updated

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
         <div>
             <div className="card bg-base-100 w-96 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
+                    <h2 className="card-title">Image Optimization</h2>
                     {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
                 </div>
                 <figure>
