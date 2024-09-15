51b13870bf4b68531b2db403ff11f23262b185e1
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-16 01:11:37 +0600

    img height and width updated

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h1 className=''>Gallery Page</h1>
+            <h1 className='text-3xl'>Gallery Page</h1>
             <div className="card bg-base-100 w-96 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title">Image Optimization</h2>
                     {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
                 </div>
                 <figure>
-                    <Image width={600} height={500} src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    <Image width="1920" height="1080" src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
                     alt="Shoes"></Image>
                 </figure>
             </div>
